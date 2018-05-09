52858d980631d766d0b42600274e52e2d1d8c8d1
kirchmann <carl.christian.kirchmann@gmail.com>
2018-05-09 21:39:15 +0200

    Things were not working properly, rm rf and found another solution online and copy pasted.

--- serialtest/serialtest.ino
+++ serialtest/serialtest.ino
@@ -1,314 +1,149 @@
-#include <FastLED.h>
-// LCD Defintitions
-#define PIN_RESET 12  // LCD RST .... Pin 1
-#define PIN_SCE   11  // LCD CS  .... Pin 3
-#define PIN_DC    10  // LCD Dat/Com. Pin 5
-#define PIN_SDIN  9  // LCD SPIDat . Pin 6
-#define PIN_SCLK  8  // LCD SPIClk . Pin 4
+#include "FastLED.h"
 
-#define LCD_C     LOW
-#define LCD_D     HIGH
+#define NUM_LEDS (20)
+#define LED_DATA_PIN 7
+#define NUM_BYTES (NUM_LEDS*3) // 3 colors  
 
-#define LCD_X     84
-#define LCD_Y     48
-#define LCD_CMD   0
+#define BRIGHTNESS 32
+#define UPDATES_PER_SECOND 60
 
-// LED PINS
-#define DATA_PIN 7;    // data pin to neopixels, Green: datapin, white: GND, red: power (5V)
+#define TIMEOUT 3000
 
-const int NUM_LEDS = 10;//60; // Number of leds, 15 + 30 + 15
+#define MODE_ANIMATION 0
+#define MODE_AMBILIGHT 1
+#define MODE_BLACK 2
+uint8_t mode = MODE_ANIMATION;
+
+byte MESSAGE_PREAMBLE[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09 };
+uint8_t PREAMBLE_LENGTH = 10;
+uint8_t current_preamble_position = 0;
+
+unsigned long last_serial_available = -1L;
+
+int led_counter = 0;
+int byte_counter = 0;
+
 CRGB leds[NUM_LEDS];
-const int ledPin = 13;
-const byte numBytes = 31;//180 //NUM_LEDS*3 (R,G and B -> 3)
-byte receivedBytes[numBytes];
-byte numReceived = 0;
-boolean newData = false;
+byte buffer[NUM_BYTES];
 
-void setup() {
-  // initialize serial:
-  Serial.begin(115200);
-  FastLED.addLeds<WS2812B, 7, GRB>(leds, NUM_LEDS);
-  LcdInitialise();
-  LcdClear();
-  gotoXY(0,0);
-  LcdString ("Starting");
-  delay(1000);
-  LcdClear();
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin,LOW);
-  FastLED.setBrightness(64);
-}
+// Filler animation attributes
+CRGBPalette16 currentPalette = RainbowColors_p;
+TBlendType    currentBlending = LINEARBLEND;
+uint8_t startIndex = 0;
 
-void loop() {
-  int i;
-  LcdClear();
-  gotoXY(0,0);
-  showNewData();
-  recvWithStartEndMarkers();
-}
-
-void showNewData() {
-  if (newData == true) {
-    /*gotoXY(0,0);
-    LcdString("Received bytes #: ");
-    char cstr[16];
-    itoa(numReceived, cstr, 10);
-    LcdString(cstr);
-    digitalWrite(ledPin,HIGH);
-    LcdString ("got all data");
-    digitalWrite(ledPin,LOW);
-    */
-    digitalWrite(ledPin,HIGH);
-
-    int i,k;
-/*
-    gotoXY(0,0);
-    LcdString ("RX:");
-    char str[32] = "";// too many to print all 182
-    array_to_string(receivedBytes, numReceived, str);
-    LcdString(str);
-  */  
-    newData = false;
-    // Set R,G,B for all leds (NUM_LEDS)
-    // They arrive as: R,G,B,R,G,B,R,G,B
-    // I believe the first 15 LEDS are set in incorrect order 
-    // Tried sending constant data from Java, it does not work well. Need to send less data at a time? more startmarkers
-    // Works good with 32 bytes (startmarker + data for 10 LEDS + endmarker)
-    i = 0;
-    for(k = 0; k < NUM_LEDS; k++) { 
-      leds[k].red   = receivedBytes[i];
-      leds[k].green = receivedBytes[i + 1];
-      leds[k].blue  = receivedBytes[i + 2];
-      i = i+3;
-    }
-    FastLED.setBrightness(64);
-    FastLED.show();
-    digitalWrite(ledPin,LOW);
-    //delay(10);
-} else {
-  FastLED.clear();
-  FastLED.show();
-  //delay(10);
-  int c = 10;
-/*
-  LcdString("Received bytes #: ");
-  char cstr[16];
-  itoa(numReceived, cstr, 10);
-  LcdString(cstr);
-  */
-  }
-}
-
-void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
-    int index = 0;
-    byte startMarker = 0x3C;
-    byte endMarker = 0x3E;
-
-    byte rc;
-
- // if (Serial.available() > 0) {
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
-
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedBytes[index] = rc;
-                index++;
-                if (index >= numBytes) {
-                    index = numBytes - 1;
-                }
-            }
-            else {
-                receivedBytes[index] = '\0'; // terminate the string
-                recvInProgress = false;
-                index = 0;
-                newData = true;
-            }
-        }
-
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
-    }
-}
-
-void array_to_string(byte array[], unsigned int len, char buffer[])
+void setup()
 {
-    for (unsigned int i = 0; i < len; i++)
+    Serial.begin(115200); // 115200
+    FastLED.clear(true);
+    FastLED.addLeds<WS2812B, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(BRIGHTNESS);
+}
+
+void loop()
+{
+    switch (mode) {
+        case MODE_ANIMATION: 
+            fillLEDsFromPaletteColors();
+            break;
+        
+        case MODE_AMBILIGHT:
+            processIncomingData();
+            break;
+
+            case MODE_BLACK:
+            showBlack();
+            break;
+    }
+    
+}
+
+void processIncomingData()
+{
+    if (waitForPreamble(TIMEOUT))
     {
-        byte nib1 = (array[i] >> 4) & 0x0F;
-        byte nib2 = (array[i] >> 0) & 0x0F;
-        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+        Serial.readBytes((char*)buffer, NUM_BYTES);
+
+        while (byte_counter < NUM_BYTES)
+        {
+            byte blue = buffer[byte_counter++];
+            byte green = buffer[byte_counter++];
+            byte red = buffer[byte_counter++];
+            
+            leds[led_counter++] = CRGB(red, green, blue);
+        }
+
+        FastLED.show();
+        
+        // flush the serial buffer to avoid flickering
+        while(Serial.available()) { Serial.read(); } 
+        
+        byte_counter = 0;
+        led_counter = 0;
     }
-    buffer[len*2] = '\0';
+    else
+    {
+        //if we get here, there must have been data before(so the user already knows, it works!)
+        //simply go to black!
+        mode = MODE_BLACK;  
+    }
 }
 
-static const byte ASCII[][5] =
+bool waitForPreamble(int timeout)
 {
- {0x00, 0x00, 0x00, 0x00, 0x00} // 20  
-,{0x00, 0x00, 0x5f, 0x00, 0x00} // 21 !
-,{0x00, 0x07, 0x00, 0x07, 0x00} // 22 "
-,{0x14, 0x7f, 0x14, 0x7f, 0x14} // 23 #
-,{0x24, 0x2a, 0x7f, 0x2a, 0x12} // 24 $
-,{0x23, 0x13, 0x08, 0x64, 0x62} // 25 %
-,{0x36, 0x49, 0x55, 0x22, 0x50} // 26 &
-,{0x00, 0x05, 0x03, 0x00, 0x00} // 27 '
-,{0x00, 0x1c, 0x22, 0x41, 0x00} // 28 (
-,{0x00, 0x41, 0x22, 0x1c, 0x00} // 29 )
-,{0x14, 0x08, 0x3e, 0x08, 0x14} // 2a *
-,{0x08, 0x08, 0x3e, 0x08, 0x08} // 2b +
-,{0x00, 0x50, 0x30, 0x00, 0x00} // 2c ,
-,{0x08, 0x08, 0x08, 0x08, 0x08} // 2d -
-,{0x00, 0x60, 0x60, 0x00, 0x00} // 2e .
-,{0x20, 0x10, 0x08, 0x04, 0x02} // 2f /
-,{0x3e, 0x51, 0x49, 0x45, 0x3e} // 30 0
-,{0x00, 0x42, 0x7f, 0x40, 0x00} // 31 1
-,{0x42, 0x61, 0x51, 0x49, 0x46} // 32 2
-,{0x21, 0x41, 0x45, 0x4b, 0x31} // 33 3
-,{0x18, 0x14, 0x12, 0x7f, 0x10} // 34 4
-,{0x27, 0x45, 0x45, 0x45, 0x39} // 35 5
-,{0x3c, 0x4a, 0x49, 0x49, 0x30} // 36 6
-,{0x01, 0x71, 0x09, 0x05, 0x03} // 37 7
-,{0x36, 0x49, 0x49, 0x49, 0x36} // 38 8
-,{0x06, 0x49, 0x49, 0x29, 0x1e} // 39 9
-,{0x00, 0x36, 0x36, 0x00, 0x00} // 3a :
-,{0x00, 0x56, 0x36, 0x00, 0x00} // 3b ;
-,{0x08, 0x14, 0x22, 0x41, 0x00} // 3c <
-,{0x14, 0x14, 0x14, 0x14, 0x14} // 3d =
-,{0x00, 0x41, 0x22, 0x14, 0x08} // 3e >
-,{0x02, 0x01, 0x51, 0x09, 0x06} // 3f ?
-,{0x32, 0x49, 0x79, 0x41, 0x3e} // 40 @
-,{0x7e, 0x11, 0x11, 0x11, 0x7e} // 41 A
-,{0x7f, 0x49, 0x49, 0x49, 0x36} // 42 B
-,{0x3e, 0x41, 0x41, 0x41, 0x22} // 43 C
-,{0x7f, 0x41, 0x41, 0x22, 0x1c} // 44 D
-,{0x7f, 0x49, 0x49, 0x49, 0x41} // 45 E
-,{0x7f, 0x09, 0x09, 0x09, 0x01} // 46 F
-,{0x3e, 0x41, 0x49, 0x49, 0x7a} // 47 G
-,{0x7f, 0x08, 0x08, 0x08, 0x7f} // 48 H
-,{0x00, 0x41, 0x7f, 0x41, 0x00} // 49 I
-,{0x20, 0x40, 0x41, 0x3f, 0x01} // 4a J
-,{0x7f, 0x08, 0x14, 0x22, 0x41} // 4b K
-,{0x7f, 0x40, 0x40, 0x40, 0x40} // 4c L
-,{0x7f, 0x02, 0x0c, 0x02, 0x7f} // 4d M
-,{0x7f, 0x04, 0x08, 0x10, 0x7f} // 4e N
-,{0x3e, 0x41, 0x41, 0x41, 0x3e} // 4f O
-,{0x7f, 0x09, 0x09, 0x09, 0x06} // 50 P
-,{0x3e, 0x41, 0x51, 0x21, 0x5e} // 51 Q
-,{0x7f, 0x09, 0x19, 0x29, 0x46} // 52 R
-,{0x46, 0x49, 0x49, 0x49, 0x31} // 53 S
-,{0x01, 0x01, 0x7f, 0x01, 0x01} // 54 T
-,{0x3f, 0x40, 0x40, 0x40, 0x3f} // 55 U
-,{0x1f, 0x20, 0x40, 0x20, 0x1f} // 56 V
-,{0x3f, 0x40, 0x38, 0x40, 0x3f} // 57 W
-,{0x63, 0x14, 0x08, 0x14, 0x63} // 58 X
-,{0x07, 0x08, 0x70, 0x08, 0x07} // 59 Y
-,{0x61, 0x51, 0x49, 0x45, 0x43} // 5a Z
-,{0x00, 0x7f, 0x41, 0x41, 0x00} // 5b [
-,{0x02, 0x04, 0x08, 0x10, 0x20} // 5c ¥
-,{0x00, 0x41, 0x41, 0x7f, 0x00} // 5d ]
-,{0x04, 0x02, 0x01, 0x02, 0x04} // 5e ^
-,{0x40, 0x40, 0x40, 0x40, 0x40} // 5f _
-,{0x00, 0x01, 0x02, 0x04, 0x00} // 60 `
-,{0x20, 0x54, 0x54, 0x54, 0x78} // 61 a
-,{0x7f, 0x48, 0x44, 0x44, 0x38} // 62 b
-,{0x38, 0x44, 0x44, 0x44, 0x20} // 63 c
-,{0x38, 0x44, 0x44, 0x48, 0x7f} // 64 d
-,{0x38, 0x54, 0x54, 0x54, 0x18} // 65 e
-,{0x08, 0x7e, 0x09, 0x01, 0x02} // 66 f
-,{0x0c, 0x52, 0x52, 0x52, 0x3e} // 67 g
-,{0x7f, 0x08, 0x04, 0x04, 0x78} // 68 h
-,{0x00, 0x44, 0x7d, 0x40, 0x00} // 69 i
-,{0x20, 0x40, 0x44, 0x3d, 0x00} // 6a j 
-,{0x7f, 0x10, 0x28, 0x44, 0x00} // 6b k
-,{0x00, 0x41, 0x7f, 0x40, 0x00} // 6c l
-,{0x7c, 0x04, 0x18, 0x04, 0x78} // 6d m
-,{0x7c, 0x08, 0x04, 0x04, 0x78} // 6e n
-,{0x38, 0x44, 0x44, 0x44, 0x38} // 6f o
-,{0x7c, 0x14, 0x14, 0x14, 0x08} // 70 p
-,{0x08, 0x14, 0x14, 0x18, 0x7c} // 71 q
-,{0x7c, 0x08, 0x04, 0x04, 0x08} // 72 r
-,{0x48, 0x54, 0x54, 0x54, 0x20} // 73 s
-,{0x04, 0x3f, 0x44, 0x40, 0x20} // 74 t
-,{0x3c, 0x40, 0x40, 0x20, 0x7c} // 75 u
-,{0x1c, 0x20, 0x40, 0x20, 0x1c} // 76 v
-,{0x3c, 0x40, 0x30, 0x40, 0x3c} // 77 w
-,{0x44, 0x28, 0x10, 0x28, 0x44} // 78 x
-,{0x0c, 0x50, 0x50, 0x50, 0x3c} // 79 y
-,{0x44, 0x64, 0x54, 0x4c, 0x44} // 7a z
-,{0x00, 0x08, 0x36, 0x41, 0x00} // 7b {
-,{0x00, 0x00, 0x7f, 0x00, 0x00} // 7c |
-,{0x00, 0x41, 0x36, 0x08, 0x00} // 7d }
-,{0x10, 0x08, 0x08, 0x10, 0x08} // 7e ←
-,{0x00, 0x06, 0x09, 0x09, 0x06} // 7f →
-};
+    last_serial_available = millis();
+    current_preamble_position = 0;
+    while (current_preamble_position < PREAMBLE_LENGTH)
+    {
+        if (Serial.available() > 0)
+        {
+            last_serial_available = millis();
 
+            if (Serial.read() == MESSAGE_PREAMBLE[current_preamble_position])
+            {
+                current_preamble_position++;
+            }
+            else
+            {
+                current_preamble_position = 0;
+            }
+        }
 
-void LcdCharacter(char character)
-{
-  LcdWrite(LCD_D, 0x00);
-  for (int index = 0; index < 5; index++)
-  {
-    LcdWrite(LCD_D, ASCII[character - 0x20][index]);
-  }
-  LcdWrite(LCD_D, 0x00);
+        if (millis() - last_serial_available > timeout)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-void LcdClear(void)
+void fillLEDsFromPaletteColors()
 {
-  for (int index = 0; index < LCD_X * LCD_Y / 8; index++)
-  {
-    LcdWrite(LCD_D, 0x00);
-  }
+    startIndex++;
+
+    uint8_t colorIndex = startIndex;
+    for( int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = ColorFromPalette(currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+            colorIndex += 3;
+        }
+
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+
+    if (Serial.available() > 0)
+    {
+        mode = MODE_AMBILIGHT;
+    }
 }
 
-void LcdInitialise(void)
-{
-  pinMode(PIN_SCE,   OUTPUT);
-  pinMode(PIN_RESET, OUTPUT);
-  pinMode(PIN_DC,    OUTPUT);
-  pinMode(PIN_SDIN,  OUTPUT);
-  pinMode(PIN_SCLK,  OUTPUT);
+void showBlack()
+{     
+    for( int i = 0; i < NUM_LEDS; i++) 
+    {
+        leds[i] = CRGB(0,0,0);
+    }
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
 
-  digitalWrite(PIN_RESET, LOW);
- // delay(1);
-  digitalWrite(PIN_RESET, HIGH);
-
-  LcdWrite( LCD_CMD, 0x21 );  // LCD Extended Commands.
-  LcdWrite( LCD_CMD, 0xBf );  // Set LCD Vop (Contrast). //B1
-  LcdWrite( LCD_CMD, 0x04 );  // Set Temp coefficent. //0x04
-  LcdWrite( LCD_CMD, 0x14 );  // LCD bias mode 1:48. //0x13
-  LcdWrite( LCD_CMD, 0x0C );  // LCD in normal mode. 0x0d for inverse
-  LcdWrite(LCD_C, 0x20);
-  LcdWrite(LCD_C, 0x0C);
+    if (Serial.available() > 0)
+    {
+        mode = MODE_AMBILIGHT;
+    }
 }
-
-void LcdString(char *characters)
-{
-  while (*characters)
-  {
-    LcdCharacter(*characters++);
-  }
-}
-
-void LcdWrite(byte dc, byte data)
-{
-  digitalWrite(PIN_DC, dc);
-  digitalWrite(PIN_SCE, LOW);
-  shiftOut(PIN_SDIN, PIN_SCLK, MSBFIRST, data);
-  digitalWrite(PIN_SCE, HIGH);
-}
-
-// gotoXY routine to position cursor 
-// x - range: 0 to 84
-// y - range: 0 to 5
-
-void gotoXY(int x, int y)
-{
-  LcdWrite( 0, 0x80 | x);  // Column.
-  LcdWrite( 0, 0x40 | y);  // Row.  
-
-}
-
