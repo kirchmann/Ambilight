30231c7cb4aeb1e2c29e60a068ccb8c7bbb02f4f
kirchmann <carl.christian.kirchmann@gmail.com>
2018-09-04 17:40:34 +0200

    Added basic GUI.

---
+++ src/GUI_learning.java
@@ -0,0 +1,111 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+public class GUI_learning {
+
+	private JFrame frame;
+	private JTextField updatesPerSecondTextField;
+	private JLabel updatesPerSecondTextField_label;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_learning window = new GUI_learning();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI_learning() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+        Ambilight ambilight = new Ambilight();
+        ambilight.initialize();
+        Runnable ambilightRunnable = new Runnable() {
+            public void run() {
+              long startTime = System.nanoTime();
+              ambilight.takeScreenshot();
+              ambilight.calculateColorOfAllLEDs();
+              ambilight.flushBufferedImage();
+              ambilight.sendDataToCompPort();
+              long endTime = System.nanoTime();
+              long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+              System.out.println(duration);
+            }
+          };
+
+		JCheckBox OnOrOffButton = new JCheckBox("On/off");
+		OnOrOffButton.setBounds(20, 57, 69, 72);
+		OnOrOffButton.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent event) {
+		        JCheckBox cb = (JCheckBox) event.getSource();
+		        if (cb.isSelected()) {
+		            // do something if check box is selected
+		        	System.out.println("on");
+		            ScheduledExecutorService ambilightThread = Executors.newSingleThreadScheduledExecutor();
+		            ambilightThread.scheduleAtFixedRate(ambilightRunnable, 0, 100, TimeUnit.MILLISECONDS);
+		        } else {
+		            // check box is unselected, do something else
+		        	
+		        	System.out.println("off");
+		        }
+		        
+		    }
+		});
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		    	ambilight.disconnectComPort();
+		    	System.exit(0);
+		    }
+		});
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(OnOrOffButton);
+		
+		updatesPerSecondTextField = new JTextField();
+		updatesPerSecondTextField.setBounds(72, 22, 50, 27);
+		updatesPerSecondTextField.setText("Input");
+		updatesPerSecondTextField.setHorizontalAlignment(SwingConstants.LEFT);
+		frame.getContentPane().add(updatesPerSecondTextField);
+		updatesPerSecondTextField.setColumns(3);
+		
+		updatesPerSecondTextField_label = new JLabel("Updates / s:");
+		updatesPerSecondTextField_label.setBounds(10, 17, 69, 36);
+		updatesPerSecondTextField_label.setLabelFor(updatesPerSecondTextField);
+		frame.getContentPane().add(updatesPerSecondTextField_label);
+		
+		
+		
+		
+	}
+
+}
